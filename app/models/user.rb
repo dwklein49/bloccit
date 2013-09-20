6de5a3117f42cf29e8244edb2475fea2b7918e7d
David Klein <dwklein49@gmail.com>
2013-09-20 16:10:00 -0700

    Associated users and posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
   # attr_accessible :title, :body
+  has_many :posts
 end
