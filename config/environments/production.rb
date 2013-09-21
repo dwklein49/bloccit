62841d04f78d800be2297aa21d1012674e5831bc
David Klein <dwklein49@gmail.com>
2013-09-20 19:39:48 -0700

    Configured App for production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,3 @@ Bloccit::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.action_mailer.default_url_options = { :host => 'bloccit.herokuapp.com' }  
 end
-end
