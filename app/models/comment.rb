0c991df1a5eb430bd6b50a635758d9933e6cb5e9
David Klein <dwklein49@gmail.com>
2013-09-25 19:53:11 -0700

    Added comments to Bloccit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :user
   
-  attr_accessible :body
+  attr_accessible :body, :post
 
-  validates :body, length: {minimum: 20}, presence: true
+  validates :body, length: {minimum: 5}, presence: true
   validates :user, presence: true
 end
