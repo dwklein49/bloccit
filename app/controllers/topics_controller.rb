066e5ccf50ce0d15dd790f03630ebcb183028f11
David Klein <dwklein49@gmail.com>
2013-09-25 20:47:36 -0700

    Destroyed topics, posts and comments.

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -3,12 +3,12 @@ class TopicsController < ApplicationController
     @topics = Topic.paginate(page: params[:page], per_page: 10) 
   end
 
- def new
+  def new
     @topic = Topic.new
     authorize! :create, @topic, message: "You need to be an admin to do that."
   end
 
- def show
+  def show
     @topic = Topic.find(params[:id])
     @posts = @topic.posts.paginate(page: params[:page], per_page: 10)
   end
@@ -18,7 +18,7 @@ class TopicsController < ApplicationController
     authorize! :update, @topic, message: "You need to be an admin to do that."
   end
 
- def create
+  def create
     @topic = Topic.new(params[:topic])
     authorize! :create, @topic, message: "You need to be an admin to do that."
     if @topic.save
@@ -38,6 +38,19 @@ class TopicsController < ApplicationController
     else
       flash[:error] = "Error saving topic. Please try again"
       render :edit
+    end
+  end  
+  
+  def destroy
+    @topic = Topic.find(params[:id])
+    name = @topic.name
+    authorize! :destroy, @topic, message: "You need to own the topic to delete it."
+    if @topic.destroy
+      flash[:notice] = "\"#{name}\" was deleted successfully."
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error deleting the topic."
+      render :show
     end
   end
 end
