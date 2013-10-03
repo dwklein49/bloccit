724b9575130c157c636b8134061fbc3af2a8b24f
David Klein <dwklein49@gmail.com>
2013-10-02 18:31:34 -0700

    Implemented voting

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,12 +7,14 @@ class Post < ActiveRecord::Base
   belongs_to :user
   belongs_to :topic
 
-  default_scope order('created_at DESC')
+  default_scope order('rank DESC')
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+  after_create :create_vote
 
   mount_uploader :image, ImageUploader
 
@@ -28,4 +30,18 @@ class Post < ActiveRecord::Base
     self.votes.sum(:value).to_i
   end 
   
+  def update_rank
+    age = (self.created_at - Time.new(1970,1,1)) / 86400
+    new_rank = points + age
+
+    self.update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  # Who ever created a post, should automatically be set to "voting" it up.
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end  
+
 end
